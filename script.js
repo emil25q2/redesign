25462ca007e92877bb2a4c01445c46998decbebd
Emilie da Silva <emiliedasilva@Emilies-MacBook-Air-4.local>
2020-12-11 10:11:44 +0100

    Day 1 process

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-window.addEventListener("load", sidenVises);
-
-function sidenVises() {
-    console.log("sidenVises");
-
-    //animationer
-    document.querySelector("#hitit").classList.add("zoom_in_fade");
-
-    //hide stuff
-    document.querySelector("#header").classList.add("hide");
-    document.querySelector("video").classList.add("hide");
-    document.querySelector("#images_container").classList.add("hide");
-    document.querySelector("#my_footer").classList.add("hide");
-
-    //hitit -> forside
-    document.querySelector("#hitit").addEventListener("click", forside);
-}
-
-function forside() {
-    console.log("forside");
-
-    //hide stuff
-    document.querySelector("#hitit").classList.add("hide");
-    document.querySelector("#hitit").classList.add("hide");
-    document.querySelector("#images_container").classList.remove("hide");
-
-    //show stuff
-    document.querySelector("#header").classList.remove("hide");
-    document.querySelector("video").classList.remove("hide");
-    document.querySelector("#my_footer").classList.remove("hide");
-
-    //afspil video
-    document.querySelector("video").play();
-}
+//window.addEventListener("load", forside);
+//
+////function sidenVises() {
+////    console.log("sidenVises");
+////
+////    //animationer
+////    document.querySelector("#hitit").classList.add("zoom_in_fade");
+////
+////    //hide stuff
+////    document.querySelector("#header").classList.add("hide");
+////    document.querySelector("video").classList.add("hide");
+////    document.querySelector("#images_container").classList.add("hide");
+////    document.querySelector("#my_footer").classList.add("hide");
+////
+////    //hitit -> forside
+////    document.querySelector("#hitit").addEventListener("click", forside);
+////}
+//
+//function forside() {
+//    console.log("forside");
+//
+//    //    //hide stuff
+//    //    document.querySelector("#hitit").classList.add("hide");
+//    //    document.querySelector("#hitit").classList.add("hide");
+//    //    document.querySelector("#images_container").classList.remove("hide");
+//
+//    //show stuff
+//    document.querySelector("#header").classList.remove("hide");
+//    document.querySelector("video").classList.remove("hide");
+//    document.querySelector("#my_footer").classList.remove("hide");
+//
+//    //afspil video
+//    document.querySelector("video").play();
+//}
